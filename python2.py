a892d671319ca6b0c08e53d60845f950eec8f6de
Jose Macias <119005740+Paramorito@users.noreply.github.com>
2023-03-18 19:02:12 -0600

    Add python2.py
    This practice lets you input a parsecs value and then it calculates lightyears

---
+++ python2.py
@@ -0,0 +1,5 @@
+print("Insert Parsecs")
+first_number = input("Parsecs: ")
+parsecs = int(first_number)
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
