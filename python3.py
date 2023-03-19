ab7ee9366acd3f05ce9594cefd4d25d79ee2f0a4
Jose Macias <119005740+Paramorito@users.noreply.github.com>
2023-03-18 19:03:49 -0600

    Add python3.py
    This program calculates the greater value between 2 variables

---
+++ python3.py
@@ -0,0 +1,9 @@
+print("¿Cual es el valor mas grande?")
+firstnumber = input("Valor de A: ")
+A = int(firstnumber)
+secondnumber = input("Valor de B: ")
+B = int(secondnumber)
+# test expression
+if A < B:
+    # statement to be run
+    print(B)
